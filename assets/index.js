84d2e55c0e7cd6a87e631f2e126ef1a7f466dd2e
Karim Alibhai <karim@alibhai.co>
2016-10-23 17:48:07 -0400

    add controls

--- assets/index.js
+++ assets/index.js
@@ -15,33 +15,61 @@
         let offset = 0.5 * current.offsetHeight
 
         styles.innerHTML = `
-                .text span {
-                    position:absolute;
-                    top: ${blank.offsetTop}px;
-                }
+        .text span {
+            position:absolute;
+            top: ${blank.offsetTop}px;
+        }
 
-                .text span.past {
-                    top: ${blank.offsetTop-offset}px;
-                }
+        .text span.past {
+            top: ${blank.offsetTop-offset}px;
+        }
 
-                .text span.future {
-                    top: ${blank.offsetTop+offset}px;
-                }
-                `
+        .text span.future {
+            top: ${blank.offsetTop+offset}px;
+        }
+        `
     }
 
+    /**
+     * Iterator and handler for the words.
+     */
+    const list = document.getElementById('list')
     const words = {
         index: -1,
-        list: [
-            'passionate people',
-            'open education',
-            'self-paced learning',
-            'you'
-        ],
+        list: [],
+
+        add: function (word) {
+            let index = words.list.push(word) - 1
+
+            let li = document.createElement('li')
+            let text = document.createElement('span')
+            let btnRm = document.createElement('button')
+
+            text.innerText = word
+
+            btnRm.classList.add('remove')
+            btnRm.setAttribute('data-index', String(index))
+            btnRm.innerText = 'x'
+
+            btnRm.addEventListener('click', function (evt) {
+                evt.preventDefault()
+                words.list = words.list.filter(function (w) {
+                    return w !== word
+                })
+
+                next.innerText = words.peek() + '.'
+                list.removeChild(li)
+            })
+
+            li.appendChild(text)
+            li.appendChild(btnRm)
+
+            list.appendChild(li)
+        },
 
         nextIndex: function(i) {
             i++
-            if (i === words.list.length) i = 0
+            if (i >= words.list.length) i = 0
             return i
         },
 
@@ -53,6 +81,22 @@
             return words.list[words.nextIndex(words.index)]
         }
     }
+
+    const txtInput = document.getElementById('txtInput')
+    document.getElementById('btnAdd').addEventListener('click', function (evt) {
+        evt.preventDefault()
+
+        let text = txtInput.value
+        txtInput.value = ''
+        words.add(text)
+    })
+
+    ;[
+            'passionate people',
+            'open education',
+            'self-paced learning',
+            'you'
+    ].forEach(words.add)
 
     let animate = function() {
         updateCSS()
@@ -75,8 +119,8 @@
                 next = current
                 current = tmp
 
-                setTimeout(animate, 1000)
-            }, 3000)
+                setTimeout(animate, 500)
+            }, 800)
         }, 300)
     }
 
